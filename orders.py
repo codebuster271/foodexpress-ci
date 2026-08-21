36ce915bfa1abd685a3a2653a6b411b46f147b03
codebuster271 <223704407+codebuster271@users.noreply.github.com>
2026-08-21 08:58:02 +0530

    Corrected orders.py

--- orders.py
+++ orders.py
@@ -3,9 +3,3 @@ def order_total(cart_amount, delivery_fee):
 
 def is_free_delivery(cart_amount):
     return cart_amount >= 500
-
-<<<<<<< HEAD
-print("Option A")
-=======
-print("Option B")
->>>>>>> feature_b
